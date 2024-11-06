7d8f546051c7fec8853e8b38c7260286bf1da386
ammarsalahi <ammarsaalahi@gmail.com>
2024-11-06 21:13:57 +0330

    change database and update requirements

--- db/database.py
+++ db/database.py
@@ -1,9 +1,17 @@
 from sqlalchemy.ext.asyncio import create_async_engine,async_sessionmaker
-from sqlalchemy.orm import DeclarativeBase,MappedAsDataclass
+from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy import (
+    Column,
+    DateTime,
+    func,
+    UUID
+)
+from uuid import uuid4
 
 DATABASE_URL='sqlite+aiosqlite:///./data.db'
 
 engine =create_async_engine(DATABASE_URL)
+
 sessionLocal=async_sessionmaker(
     bind=engine,
     autoflush=False,
@@ -11,9 +19,12 @@ sessionLocal=async_sessionmaker(
     expire_on_commit=False
 )
 
-class Base(DeclarativeBase,MappedAsDataclass):
-    pass
+class Base(DeclarativeBase):
+    __abstract_=True
 
+    id = Column(UUID(as_uuid=uuid4),primary_key=True,default=uuid4,index=True,unique=True)
+    created_at = Column(DateTime,default=func.now())
+    updated_at = Column(DateTime,default=func.now(),onupdate=func.now())
 async def get_db():
     db=sessionLocal()
     try:
